f55242a2c333a2728c328153572ee162bfa1b483
Wellington Barbosa <huehueb58@gmail.com>
2026-05-18 17:03:04 -0300

    fix: reorganizar importações e adicionar middleware para servir arquivos estáticos

--- src/routes/uploads-routes.ts
+++ src/routes/uploads-routes.ts
@@ -1,8 +1,9 @@
 import { UploadsController } from "@/controllers/uploads-controller.js";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated.js";
 import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization.js";
-import uploadConfig from "@/configs/upload.js";
 import { Router } from "express";
+import express from "express";
+import uploadConfig from "@/configs/upload.js";
 import multer from "multer";
 
 const uploadsRoutes = Router();
@@ -14,6 +15,7 @@ uploadsRoutes.use(
   ensureAuthenticated,
   verifyUserAuthorization(["employee", "manager"]),
 );
+uploadsRoutes.use(express.static(uploadConfig.UPLOAD_FOLDER));
 uploadsRoutes.post("/", upload.single("file"), uploadsController.upload);
 
 export { uploadsRoutes };
